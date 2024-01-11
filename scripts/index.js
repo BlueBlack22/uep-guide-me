42d7e972ac6dbcc76da993598865ace2f1a2bff1
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2024-01-11 21:12:33 +0100

    update

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+document
+  .querySelector("#login")
+  .addEventListener("click", (e) => (window.location = "./pages/login.html"));
--- scripts/login.js
+++ scripts/login.js
@@ -1,14 +1,43 @@
-function validateForm() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+userDB = [
+  {
+    username: "jan.kowalski",
+    password: "12345",
+    guide: false,
+  },
+  {
+    username: "adam.nowak",
+    password: "12345",
+    guide: true,
+  },
+];
 
-  // Simple validation
-  if (username === "" || password === "") {
+function validateForm() {
+  let name = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+  let userFound = false;
+
+  if (name === "" || pass === "") {
     alert("Username and password are required.");
     return false;
   }
 
-  // You can add more complex validation logic here.
+  for (let i in userDB) {
+    if (userDB[i].username == name) {
+      userFound = true;
+      if (userDB[i].password == pass) {
+        window.location = "./tourist-home.html";
+      } else {
+        alert("Password is wrong");
+      }
+    }
+  }
 
-  return true;
+  if (userFound == false) {
+    alert("Account not in database");
+  }
 }
+
+document.querySelector("#loginForm").addEventListener("submit", (e) => {
+  validateForm();
+  e.preventDefault();
+});

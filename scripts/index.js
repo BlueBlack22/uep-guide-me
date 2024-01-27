c1a35da9ca3dfc38992343bd1bde8fbf918274f9
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2024-01-27 20:19:48 +0100

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
 document
   .querySelector("#login")
   .addEventListener("click", (e) => (window.location = "./pages/login.html"));
+
+document.querySelector("#guide").addEventListener("click", () => {
+  window.location = "./pages/register-guide.html";
+});
+
+document.querySelector("#tourist").addEventListener("click", () => {
+  window.location = "./pages/register-tourist.html";
+});
